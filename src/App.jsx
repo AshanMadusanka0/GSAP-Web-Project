3f50b6a50da668c46e827acd3651f1794de5e3a2
Ashan_madusanka <136890819+AshanMadusanka0@users.noreply.github.com>
2025-08-01 01:25:58 +0530

    add telwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import {ScrollTrigger,SplitText} from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
 
 const App = () => {
   return (
